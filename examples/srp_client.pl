a3edd8284cf3beeed8ba62f90f3cd0cf301481cb
Karel Miko <miko@dcit.cz>
2013-04-05 04:45:40 +0200

    BASE64 v demo prikladech

--- examples/srp_client.pl
+++ examples/srp_client.pl
@@ -3,9 +3,12 @@
 use strict;
 use warnings;
 use Mojo::UserAgent;
+use Mojo::Util qw(b64_encode b64_decode);
 use Crypt::SRP;
 
+
 my $ua = Mojo::UserAgent->new;
+$ua->http_proxy('http://127.0.0.1:8081');
 
 my $base_url = 'http://127.0.0.1:3000';
 
@@ -26,13 +29,13 @@ for (@test_set) {
   warn "A = ", unpack("H*", substr($A,0,16)), "... len=", length($A), "\n";
   warn "a = ", unpack("H*", substr($a,0,16)), "... len=", length($a), "\n";
   warn ">> gonna send request 1\n\n";
-  my $tx1 = $ua->post("$base_url/auth/srp_step1" => json => {I=>$I, A=>$A});
+  my $tx1 = $ua->post("$base_url/auth/srp_step1" => json => {I=>b64_encode($I), A=>b64_encode($A)});
 
   warn "# [login=$I] SRP step2\n";
 
-  my $s = $tx1->res->json->{s};
-  my $B = $tx1->res->json->{B};  
-  my $token = $tx1->res->json->{token};
+  my $s = b64_decode $tx1->res->json->{s};
+  my $B = b64_decode $tx1->res->json->{B};  
+  my $token = b64_decode $tx1->res->json->{token};
   $cli->validate_A_or_B($B) or warn("invalid B") and next;
   $cli->client_init($I, $P, $s, $B);
   my $M1 = $cli->client_compute_M1();
@@ -41,11 +44,11 @@ for (@test_set) {
   warn "B = ", unpack("H*", substr($B,0,16)), "... len=", length($B), "\n";
   warn "M1= ", unpack("H*", $M1), "\n";
   warn ">> gonna send request 2\n\n";
-  my $tx2 = $ua->post("$base_url/auth/srp_step2" => json => {M1=>$M1, token=>$token});
+  my $tx2 = $ua->post("$base_url/auth/srp_step2" => json => {M1=>b64_encode($M1), token=>b64_encode($token)});
 
   warn "# [login=$I] SRP result\n";
 
-  my $M2 = $tx2->res->json->{M2};
+  my $M2 = b64_decode $tx2->res->json->{M2};
   warn "M2= ", unpack("H*", $M2), "\n" if $M2;
   if ($M2 && $cli->client_verify_M2($M2)) {
      my $K = $cli->get_secret_K; # shared secret
--- examples/srp_server.pl
+++ examples/srp_server.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 use Mojolicious::Lite;
-
+use Mojo::Util qw(b64_encode b64_decode);
 use Crypt::SRP;
 use Digest::SHA qw(hmac_sha256);
 
@@ -30,8 +30,8 @@ for (1..3) {
 
 post '/auth/srp_step1' => sub {
     my $self = shift;
-    my $I = $self->req->json->{I};
-    my $A = $self->req->json->{A};
+    my $I = b64_decode $self->req->json->{I};
+    my $A = b64_decode $self->req->json->{A};
     return $self->render_json({status=>'invalid'}) unless $I && $A;
     my $srv = Crypt::SRP->new('RFC5054-1024bit', 'SHA1');
     return $self->render_json({status=>'invalid'}) unless $srv->validate_A_or_B($A);
@@ -46,7 +46,7 @@ post '/auth/srp_step1' => sub {
       warn "b = ", unpack("H*", substr($b,0,16)), "... len=", length($b), "\n";
       my $token = $srv->random_bytes(32);
       $TOKENS{$token} = [$I, $A, $B, $b];
-      return $self->render_json({B=>$B, s=>$s, token=>$token});
+      return $self->render_json({B=>b64_encode($B), s=>b64_encode($s), token=>b64_encode($token)});
     }
     else {
       # fake response for no-nexisting user
@@ -59,8 +59,8 @@ post '/auth/srp_step1' => sub {
 
 post '/auth/srp_step2' => sub {
     my $self = shift;
-    my $M1 = $self->req->json->{M1};
-    my $token = $self->req->json->{token};
+    my $M1 = b64_decode $self->req->json->{M1};
+    my $token = b64_decode $self->req->json->{token};
     return $self->render_json({status=>'error'}) unless $M1 && $token && $TOKENS{$token};
     my $M2 = '';
     my ($I, $A, $B, $b) = @{delete $TOKENS{$token}};
@@ -71,7 +71,7 @@ post '/auth/srp_step2' => sub {
     $srv->server_init($I, $v, $s, $A, $B, $b);
     return $self->render_json({status=>'error'}) unless $srv->server_verify_M1($M1);
     $M2 = $srv->server_compute_M2;
-    return $self->render_json({M2=>$M2});
+    return $self->render_json({M2=>b64_encode($M2)});
   };
 
 app->start;
